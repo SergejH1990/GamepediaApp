e102cf6c2e80fe8766cbad6c3c261b54a1ccb1de
SergejH1990 <seho1990@gmx.de>
2025-12-10 19:19:00 +0100

    added description and platform to details

--- modules/game/ui/src/commonMain/kotlin/com/sermut/game/ui/gamedetails/GameDetailsScreen.kt
+++ modules/game/ui/src/commonMain/kotlin/com/sermut/game/ui/gamedetails/GameDetailsScreen.kt
@@ -1,11 +1,21 @@
 package com.sermut.game.ui.gamedetails
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,6 +23,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -31,7 +43,10 @@ fun GameDetailsScreen(
         viewModel.GetGameDetails(id.toInt())
     }
 
-    GameDetailsScreenContent(modifier = Modifier.fillMaxSize(), uiState = uiState.value)
+    GameDetailsScreenContent(
+        modifier = Modifier.fillMaxSize(),
+        uiState = uiState.value
+    )
 }
 
 @Composable
@@ -62,7 +77,9 @@ fun GameDetailsScreenContent(
             modifier = Modifier.fillMaxSize(),
         ){
             LazyColumn(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 50.dp)
             ){
                 item {
                     AsyncImage(
@@ -83,6 +100,68 @@ fun GameDetailsScreenContent(
                         text = data.name,
                         style = MaterialTheme.typography.headlineSmall
                     )
+                }
+
+                item {
+                    Text(
+                        text = data.description,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .padding(horizontal = 12.dp, vertical = 8.dp)
+                            .fillMaxWidth()
+                    )
+                }
+
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                                text = "Platforms",
+                                style = MaterialTheme.typography.headlineSmall,
+                                modifier = Modifier
+                                    .padding(horizontal = 12.dp)
+                                    .padding(top = 16.dp)
+                            )
+
+                        LazyRow(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            items(items = data.platforms){
+                                Card(
+                                    modifier = Modifier
+                                        .wrapContentSize()
+                                        .padding(12.dp)
+                                        .background(color = Color.White),
+                                    shape = RoundedCornerShape(12.dp),
+                                    elevation = CardDefaults.cardElevation(6.dp)
+                                ){
+                                    Column(
+                                        modifier = Modifier
+                                            .width(150.dp)
+                                    ) {
+                                        AsyncImage(
+                                            model = it.image,
+                                            contentDescription = null,
+                                            modifier = Modifier
+                                                .background(
+                                                    color = Color.Transparent,
+                                                    shape = CircleShape
+                                                ).clip(CircleShape)
+                                        )
+
+                                        Text(
+                                            text = it.name,
+                                            style = MaterialTheme.typography.titleSmall,
+                                            modifier = Modifier
+                                                .padding(vertical = 12.dp)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
