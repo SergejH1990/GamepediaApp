edff3948fb38591bd05ee08ecf76c2bc73159eee
SergejH1990 <seho1990@gmx.de>
2025-12-10 20:50:26 +0100

    added sqldelight queries

--- modules/coredatabase/build.gradle.kts
+++ modules/coredatabase/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.sql.delight)
 }
 
 kotlin {
@@ -32,6 +33,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.sql.delight.android)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -42,6 +44,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.sql.delight.common)
+            api(libs.sql.delight.common.coroutines)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -49,6 +53,7 @@ kotlin {
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutinesSwing)
+            implementation(libs.sql.delight.desktop)
         }
     }
 }
